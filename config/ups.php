a1ec2b898a3d6a8d8f9b4f5950bce87d6dcc0b5d
Adam Lee <adam@codelabx.ltd>
2023-10-04 15:17:24 +0100

    fix: Incorrect name for sandbox property

--- config/ups.php
+++ config/ups.php
@@ -3,7 +3,7 @@
 return [
     'client_id'     => env('UPS_CLIENT_ID'),
     'client_secret' => env('UPS_CLIENT_SECRET'),
-    'is_sandbox'    => env('UPS_SANDBOX'),
+    'is_sandbox'    => env('UPS_SANDBOX_MODE'),
     'shipper'       => [
         'shipper_number' => env('UPS_SHIPPER_NUMBER'),
         'address'        => [
