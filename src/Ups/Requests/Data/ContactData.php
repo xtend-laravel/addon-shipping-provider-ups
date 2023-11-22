338b1dec7ea23413c94fc9b3a1e5f30dac267722
Adam Lee <adam@codelabx.ltd>
2023-11-22 00:51:42 +0000

    fix: ContactData name not required

--- src/Ups/Requests/Data/ContactData.php
+++ src/Ups/Requests/Data/ContactData.php
@@ -7,7 +7,7 @@ use Spatie\LaravelData\Data;
 class ContactData extends Data
 {
     public function __construct(
-        public string $Name,
+        public ?string $Name,
         public ?string $AttentionName,
         public ?string $TaxIdentificationNumber,
         public ?array $Phone,
